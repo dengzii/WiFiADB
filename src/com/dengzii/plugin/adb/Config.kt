43e346ce3c29666a7dae3933862b90e28b9bfaac
dengzii <dengzii@foxmail.com>
2020-10-17 00:00:15 +0800

    clear configs

--- src/com/dengzii/plugin/adb/Config.kt
+++ src/com/dengzii/plugin/adb/Config.kt
@@ -1,6 +1,5 @@
 package com.dengzii.plugin.adb
 
-import com.google.gson.Gson
 import com.intellij.ide.util.PropertiesComponent
 
 /**
@@ -20,7 +19,7 @@ object Config {
     private const val KEY_MAIN_DIALOG_CONFIG = "com.dengzii.plugin.adb.config.dialog"
     private const val KEY_ABD_PATH = "com.dengzii.plugin.adb.config.adb"
 
-    fun clearDeviceInfos() {
+    fun clearDeviceList() {
         PropertiesComponent.getInstance().unsetValue(KEY_DEVICES_LIST)
     }
 
